363e94fa4b12ec0ba91eaf19388f0984ae6ee95e
Marou67 <91311758+Marou67@users.noreply.github.com>
2023-02-28 20:55:56 +0100

    .

---
+++ SlnLes03BestandenExcepties/WpfCopy/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace WpfCopyVs2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string info;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoice_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            string chosenFileName;
+            if (dialog.ShowDialog() == true)
+            {
+                // user picked a file and pressed OK
+                chosenFileName = dialog.FileName;
+                txtbIn.Text = chosenFileName;
+                btnGo.IsEnabled = true;
+                info = File.ReadAllText(chosenFileName);
+            }
+            else
+            {
+                // user cancelled or escaped dialog window
+            }
+
+        }
+
+        private void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|*.TXT;*.TEXT";
+            dialog.FileName = "savedfile.txt";
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, info);
+            }
+            else
+            {
+                // user pressed Cancel or escaped dialog window
+            }
+        }
+    }
+}
---
+++ SlnLes03BestandenExcepties/WpfFileInfo/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFileInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        string inhoud = null;
+        string fileName;
+        int len = 0;
+        int quantity = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChoice_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.Filter = "Tekstbestanden|.TXT;.TEXT";
+
+            if (dialog.ShowDialog() == true)
+            {
+
+                fileName = dialog.FileName;
+                using (StreamReader reader = File.OpenText(fileName))
+                {
+
+                    inhoud = reader.ReadToEnd();
+
+                    for (int i = 0; i < inhoud.Length; i++)
+                    {
+                        if (inhoud[len] == ' ' || inhoud[len] == '\t' || inhoud[len] == '\n')
+                        {
+                            quantity = quantity + 1;
+                        }
+
+                        len = len + 1;
+                    }
+
+
+                    FileInfo info = new FileInfo(fileName);
+                    lblBestand.Content = info.Name;
+                    lblWoorden.Content = quantity;
+                    lblExtensie.Content = System.IO.Path.GetExtension(fileName);
+                    lblGemaakt.Content = File.GetCreationTime(fileName);
+                    lblMapnaam.Content = System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(fileName));
+                }
+
+            }
+        }
+    }
+}
+
